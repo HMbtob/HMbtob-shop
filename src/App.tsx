3aa5dc6cdc47bb12f9950480b24b7dd4c8929d92
KKH <kkh@KKHui-MacBookPro.local>
2021-11-22 16:44:16 +0900

    2021/11/22 login, signup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Login } from './components/login';
 import { AuthContext, useAuth } from './hooks/useAuth';
-
+import { Routes, Route } from 'react-router-dom';
+import { SignUp } from './components/signup';
 function App() {
   const authContext = useAuth();
   return (
     <AuthContext.Provider value={authContext}>
-      {authContext?.authState.isLoading ? 'hi' : <Login />}
+      {authContext?.authState.isLoading ? (
+        <div>스피너</div>
+      ) : authContext?.authState.authUser ? (
+        <button onClick={() => authContext?.authHandler.logOut()}>logout</button>
+      ) : (
+        <Routes>
+          <Route path="/" caseSensitive={false} element={<Login />} />
+          <Route path="/signup" caseSensitive={false} element={<SignUp />} />
+        </Routes>
+      )}
     </AuthContext.Provider>
   );
 }
