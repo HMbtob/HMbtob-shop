cc49bba9efdd6de72307a3c76ad94a094a27b979
KKH <kkh@KKHui-MacBookPro.local>
2021-11-23 10:24:48 +0900

    Header/Search need more

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Login } from './components/login';
 import { AuthContext, useAuth } from './hooks/useAuth';
 import { Routes, Route } from 'react-router-dom';
 import { SignUp } from './components/signup';
+import { Header } from './components/header';
+import { useState } from 'react';
 function App() {
   const authContext = useAuth();
+  const [query, setQuery] = useState<string>('');
+  console.log(query);
   return (
     <AuthContext.Provider value={authContext}>
       {authContext?.authState.isLoading ? (
         <div>스피너</div>
       ) : authContext?.authState.authUser ? (
-        <button onClick={() => authContext?.authHandler.logOut()}>logout</button>
+        <>
+          <Header setQuery={setQuery} />
+        </>
       ) : (
         <Routes>
           <Route path="/" caseSensitive={false} element={<Login />} />
---
+++ src/components/header/index.tsx
@@ -0,0 +1,32 @@
+import { useAuth } from '../../hooks/useAuth';
+import { useNavigate } from 'react-router';
+import { Search } from '../search';
+import { setQueryProps } from '../../models/search';
+
+export function Header({ setQuery }: setQueryProps) {
+  const navigate = useNavigate();
+  const authContext = useAuth();
+  return (
+    <div className="bg-blue-900 w-screen p-1 absolute top-0 z-30 flex flex-row justify-between items-center">
+      <div className="flex flex-row items-center">
+        <div
+          className="text-gray-200 font-bold text-left mr-10 cursor-pointer w-20 pl-5"
+          onClick={() => navigate('/')}>
+          INTERASIA
+        </div>
+        <Search setQuery={setQuery} />
+      </div>
+      {/* 버튼들 */}
+      <div className="pr-5 flex flex-row">
+        <div
+          onClick={() => {
+            authContext?.authHandler.logOut();
+            navigate('/');
+          }}
+          className="text-sm font-mono font-bold text-center text-gray-200 bg-blue-900 cursor-pointer">
+          Logout
+        </div>
+      </div>
+    </div>
+  );
+}
