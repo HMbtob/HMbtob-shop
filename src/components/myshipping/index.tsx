7d9691fcc802b04890a4d11b87b37bce6cfa5e3f
KKH <kkh@KKHui-MacBookPro.local>
2021-12-02 15:36:02 +0900

    a

---
+++ src/components/myshipping/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export function MyShipping() {
+  return (
+    <div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+      <div>ship</div>
+    </div>
+  );
+}
